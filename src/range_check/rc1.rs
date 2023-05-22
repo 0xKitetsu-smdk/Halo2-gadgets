d929403587fd9cdf4d6cde20a8d18cbba503b7d9
0xdineshkumarsm <105421120+0xdineshkumarsm@users.noreply.github.com>
2023-05-22 13:05:30 +0530

    added tests

--- src/range_check/rc1.rs
+++ src/range_check/rc1.rs
@@ -56,3 +56,62 @@ impl<F:FieldExt,const RANGE:usize> RangeCheckConfig<F,RANGE> {
     }
 }
 
+#[cfg(test)] 
+mod tests {
+    use halo2_proofs::{
+        circuit::floor_planner::V1,
+        dev::{FailureLocation,MockProver,VerifyFailure},
+        pasta::Fp,plonk::{Circuit,Any}
+    };
+
+    use super::*;
+
+    #[derive(Default)]
+    struct MyCircuit<F:FieldExt, const RANGE: usize> {
+        value : Value<Assigned<F>>
+    }
+
+    impl <F:FieldExt,const RANGE:usize> Circuit<F> for MyCircuit<F,RANGE> {
+        type Config = RangeCheckConfig<F,RANGE>;
+        type FloorPlanner = V1;
+
+        fn without_witnesses(&self) -> Self {
+            Self::default()
+        }
+
+        fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+            let value = meta.advice_column();
+            RangeCheckConfig::configure(meta, value)
+        }
+
+        fn synthesize(&self, config: Self::Config,mut layouter: impl Layouter<F>) -> Result<(), Error> {
+            config.assign(layouter.namespace(|| "Assign Value"), self.value)?;
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_range_check(){
+        let k = 4;
+        const RANGE: usize = 8;
+
+        // Successful cases
+        for i in 0..RANGE{
+            let circuit = MyCircuit::<Fp,RANGE>{
+                value:Value::known(Fp::from(i as u64).into()),
+            };
+
+            let prover = MockProver::run(k, &circuit, vec![]).unwrap();
+            prover.assert_satisfied();
+        }
+
+        // out-of-range check, v = 8
+        let circuit = MyCircuit::<Fp,RANGE>{
+            value:Value::known(Fp::from(RANGE as u64).into()),
+        };
+
+        let prover = MockProver::run(k, &circuit, vec![]).unwrap();
+        prover.assert_satisfied();
+
+    }
+}
